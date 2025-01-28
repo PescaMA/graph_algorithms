66754ccf2d2392ae6d6882b66b724395c776b594
PescaMA <pescariu@real.cpp>
2025-01-28 09:55:06 +0200

    add past solutions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <stack>
 #include <algorithm>
 #include <cmath>
+#include <bits/stdc++.h>
+
 
 using std::vector;
 using std::pair;
@@ -120,6 +122,266 @@ void runDFS(){
     g.dfs(thestart);
 }
 */
+
+namespace SortareTopologica{
+    /// 	05 Februarie 2022
+using namespace std;
+ifstream fin("topsort.in");
+ofstream fout("topsort.out");
+int const N=100005;
+vector <int> v[N];
+int n,m,grd[N];
+void top()
+{
+    queue <int> q;
+    for(int i=1;i<=n;i++)
+        if(!grd[i])
+            {q.push(i);
+            fout<<i<<' ';}
+    while(q.size())
+    {
+        int nod=q.front();
+        vector <int>::iterator it;
+        for(it=v[nod].begin();it!=v[nod].end();it++)
+        {
+            grd[*it]--;
+            if(!grd[*it])
+            {
+
+                q.push(*it);
+                if(!grd[*it])
+                    fout<<*it<<' ';
+            }
+        }
+        q.pop();
+    }
+}
+void run()
+{
+    fin>>n>>m;
+    while(m--)
+    {
+        int x,y;
+        fin>>x>>y;
+        grd[y]++;
+
+        v[x].push_back(y);
+    }
+    top();
+}
+}
+
+namespace RoyFloyd{
+    /// 	17 Martie 2022
+using namespace std;
+int const N=105,inf=(int)1e9;
+int a[N][N],n;
+ifstream fin("roy-floyd.in");
+ofstream fout("roy-floyd.out");
+void aa(int k,int i,int j)
+{
+    if(a[i][k]+a[k][j]<a[i][j])
+        a[i][j]=a[i][k]+a[k][j];
+}
+void RF()
+{
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            if(a[i][j]==0 && i!=j)
+                a[i][j]=inf;
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                aa(k,i,j);
+}
+void afs()
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+            if(a[i][j]==inf)
+                fout<<"-1 ";
+            else
+                fout<<a[i][j]<<' ';
+        fout<<'\n';
+    }
+}
+void run()
+{
+    int m;
+    fin>>n>>m;
+    while(m--)
+    {
+        int x,y,c;
+        fin>>x>>y>>c;
+        a[x][y]=c;
+    }
+    RF();
+    afs();
+}
+}
+
+namespace Dijkstra{
+    /// 2022-03-24
+using namespace std;
+int const N=105;
+ifstream fin("dijkstra.in");
+ofstream fout("dijkstra.out");
+int n,nx;
+vector <pair<int,int>> v[N];
+priority_queue <pair<int,int>> h;
+int dp[N];
+void solve(int start)
+{
+    fill(dp,dp+n+1,INT_MAX);
+    dp[start]=0;
+    h.push({0,start});
+    while(!h.empty())
+    {
+        int cost=-h.top().first;
+        int nod=h.top().second;
+        h.pop();
+        for(auto y:v[nod])
+        {
+            if(dp[y.first]>cost+y.second)
+            {
+                dp[y.first]=cost+y.second;
+                h.push({-dp[y.first],y.first});
+            }
+        }
+    }
+}
+void run()
+{
+    fin>>n>>nx;
+    int x,y,c;
+    while(fin>>x>>y>>c)
+        v[x].push_back({y,c});
+    solve(nx);
+    for(int i=1;i<=n;i++)
+    {
+        if(dp[i]==INT_MAX)
+            fout<<"-1 ";
+        else
+            fout<<dp[i]<<' ';
+    }
+}
+}
+
+namespace bfs{
+/// 05 Februarie 2022, pbinfo
+using namespace std;
+ifstream fin("BFS.in");
+ofstream fout("BFS.out");
+int const N=(int)1e5+5;
+vector <int> v[N];
+int n,m,s,d[N];
+void BFS(int start)
+{
+    queue <int>q;
+    q.push(s);
+    fout<<s<<' ';
+    d[s]=1;
+    while(q.size())
+    {
+        int nod=q.front();
+        vector <int>::iterator it;
+        for(it=v[nod].begin();it!=v[nod].end();it++)
+        {
+            if(!d[*it])
+            {
+                fout<<*it<<' ';
+                d[*it]=d[nod]+1;
+                q.push(*it);
+            }
+        }
+        q.pop();
+    }
+}
+void run()
+{
+    fin>>n>>m>>s;
+    int x,y;
+    while(m--)
+    {
+        fin>>x>>y;
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+    for(int i=1;i<=n;i++)
+        sort(v[i].begin(),v[i].end());
+    BFS(s);
+}
+}
+
+
+/// luata de pe infoarena de la mine dar nici nu compileaza lol (si e in general f dubioasa)
+namespace domino{
+using namespace std;
+ifstream fin("domino.in");
+ofstream fout("domino.out");
+#define cin fin
+#define cout fout
+int const N =50005;
+int m;
+struct info
+{
+    int nod,pos;
+    bool intors = false;
+    info(int nod,int pos,bool intors):nod(nod),pos(pos),intors(intors){}
+    bool operator==(const info a)const{
+        return a.pos == pos;}
+};
+class hashh
+{
+    public:
+    int operator()(const info &val)const
+    {return hash<int>()(val.pos);}
+};
+unordered_multiset<info,hashh> v[22];
+bool viz[22];
+void dfs(int node,int pos=-1,bool type=false)
+{
+    while(!v[node].empty())
+    {
+        info del = *(v[node].begin());
+        v[node].erase(del);
+        v[del.nod].erase(del);
+        dfs(del.nod,del.pos,!del.intors);
+    }
+    if(pos != -1)
+        cout << pos <<' '<< type <<'\n';
+}
+int run()
+{
+    cin>>m;
+    int alfa = -1;
+    for(int i=0;i<m;i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        v[x].emplace(y,i+1,false);
+        v[y].emplace(x,i+1,true);
+        alfa = x;
+    }
+    int nr1=-1,nr2=-1;
+    for(int i=0;i<20;i++)
+        if(v[i].size()%2)
+        {
+            if(nr1 == -1) nr1 = i;
+            else if(nr2 == -1)nr2 = i;
+            else{cout << 0;return 0;}
+        }
+    if(nr2 == -1 && nr1!= -1){cout << 0;return 0;}
+    cout << "1\n";
+    if(nr1==-1)
+        dfs(alfa);
+    else
+        dfs(nr1);
+    return 0;
+}
+}
+
 
 namespace hamilton{
 
